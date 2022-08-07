cde0d18976064abe3d93a98991b83e34530db390
Shaleel Ahmed <shaleelahmed@Shaleels-MacBook-Air.local>
2022-08-07 12:45:50 +0530

    global pkg added

--- back-end/index.js
+++ back-end/index.js
@@ -19,10 +19,10 @@ app.use("/message", MessageRoutes);
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../front-end/build")));
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../front-end", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "/front-end", "build", "index.html"))
   );
 }
 // --------------------------deployment------------------------------
